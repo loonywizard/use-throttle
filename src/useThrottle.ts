18f19d8a00b9b09c038346f221099f3c0c28c0a4
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-04-05 17:02:41 +0300

    publish react-use-throttle to NPM

--- src/useThrottle.ts
+++ src/useThrottle.ts
@@ -1,24 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react"
 
 function useThrottle<T>(value: T, interval = 500): T {
-  const [throttledValue, setThrottledValue] = useState<T>(value);
-  const lastExecuted = useRef<number>(Date.now());
+  const [throttledValue, setThrottledValue] = useState<T>(value)
+  const lastExecuted = useRef<number>(Date.now())
 
   useEffect(() => {
     if (Date.now() >= lastExecuted.current + interval) {
-      lastExecuted.current = Date.now();
-      setThrottledValue(value);
+      lastExecuted.current = Date.now()
+      setThrottledValue(value)
     } else {
       const timerId = setTimeout(() => {
-        lastExecuted.current = Date.now();
-        setThrottledValue(value);
-      }, interval);
+        lastExecuted.current = Date.now()
+        setThrottledValue(value)
+      }, interval)
 
-      return () => clearTimeout(timerId);
+      return () => clearTimeout(timerId)
     }
-  }, [value, interval]);
+  }, [value, interval])
 
-  return throttledValue;
+  return throttledValue
 }
 
-export { useThrottle };
+export default useThrottle
